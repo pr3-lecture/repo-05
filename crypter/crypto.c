2d1ad445251c1a62901ad442929a54d3f7e0ac59
Jan <cppvoid@safe-mail.net>
2017-12-24 02:03:33 +0100

    Verschlüsselung vereinfacht

--- crypter/crypto.c
+++ crypter/crypto.c
@@ -7,28 +7,17 @@
 #define ENCRYPT_MODUS 0
 #define DECRYPT_MODUS 1
 
-int hasIllegalCharacters(const char* chars, const char* legalChars, int onFound)
-{
-    const size_t sizeWord = strlen(chars);
-    size_t i;
-
-    for(i = 0; i < sizeWord; ++i)
-    {
-        if(!strchr(legalChars, chars[i]))
-        {
-            return onFound;
-        }
-    }
-
-    return 0;
-}
-
-void crypt(KEY key, const char* in, char* out, int modus)
+int crypt(KEY key, const char* in, char* out, int modus)
 {
     const char* inPos;
     const char* keyPos = key.chars;
     char* outPos = out;
     int pos = -1;
+
+    if(!strlen(key.chars))
+    {
+        return E_KEY_TOO_SHORT;
+    }
 
     for(inPos = in; *inPos; ++inPos)
     {
@@ -37,14 +26,29 @@ void crypt(KEY key, const char* in, char* out, int modus)
             keyPos = key.chars;
         }
 
+        if(!strchr(KEY_CHARACTERS, *keyPos))
+        {
+            return E_KEY_ILLEGAL_CHAR;
+        }
+
         if(modus == ENCRYPT_MODUS)
         {
+            if(!strchr(MESSAGE_CHARACTERS, *inPos))
+            {
+                return E_MESSAGE_ILLEGAL_CHAR;
+            }
+
             pos = ((strchr(MESSAGE_CHARACTERS, *inPos) - (MESSAGE_CHARACTERS)) + 1) ^
                   ((strchr(KEY_CHARACTERS, *keyPos) - (KEY_CHARACTERS)) + 1);
             *outPos = CYPHER_CHARACTERS[pos];
         }
         else
         {
+            if(!strchr(CYPHER_CHARACTERS, *inPos))
+            {
+                return E_CYPHER_ILLEGAL_CHAR;
+            }
+
             pos = ((strchr(CYPHER_CHARACTERS, *inPos) - (CYPHER_CHARACTERS))) ^
                   ((strchr(KEY_CHARACTERS, *keyPos) - (KEY_CHARACTERS)) + 1);
             *outPos = MESSAGE_CHARACTERS[pos - 1];
@@ -53,62 +57,16 @@ void crypt(KEY key, const char* in, char* out, int modus)
         ++outPos;
         ++keyPos;
     }
+
+    return 0;
 }
 
 int encrypt(KEY key, const char* input, char* output)
 {
-    int inputCheck = -1;
-    int keyCheck = -1;
-
-    if(!strlen(key.chars))
-    {
-        return E_KEY_TOO_SHORT;
-    }
-
-    inputCheck = hasIllegalCharacters(input, MESSAGE_CHARACTERS, E_MESSAGE_ILLEGAL_CHAR);
-
-    if(inputCheck)
-    {
-        return inputCheck;
-    }
-
-    keyCheck = hasIllegalCharacters(key.chars, KEY_CHARACTERS, E_KEY_ILLEGAL_CHAR);
-
-    if(keyCheck)
-    {
-        return keyCheck;
-    }
-
-    crypt(key, input, output, ENCRYPT_MODUS);
-
-    return 0;
+    return crypt(key, input, output, ENCRYPT_MODUS);
 }
 
 int decrypt(KEY key, const char* cypherText, char* output)
 {
-    int inputCheck = -1;
-    int keyCheck = -1;
-
-    if(!strlen(key.chars))
-    {
-        return E_KEY_TOO_SHORT;
-    }
-
-    inputCheck = hasIllegalCharacters(cypherText, MESSAGE_CHARACTERS, E_CYPHER_ILLEGAL_CHAR);
-
-    if(inputCheck)
-    {
-        return inputCheck;
-    }
-
-    keyCheck = hasIllegalCharacters(key.chars, KEY_CHARACTERS, E_KEY_ILLEGAL_CHAR);
-
-    if(keyCheck)
-    {
-        return keyCheck;
-    }
-
-    crypt(key, cypherText, output, DECRYPT_MODUS);
-
-    return 0;
+    return crypt(key, cypherText, output, DECRYPT_MODUS);
 }
