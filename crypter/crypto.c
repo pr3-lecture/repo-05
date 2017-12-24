d2db118cd7587b56eff8c66bf0b01ba3ce61c494
Jan <cppvoid@safe-mail.net>
2017-12-24 02:14:56 +0100

    Kommentare für die Verschlüsselung

--- crypter/crypto.c
+++ crypter/crypto.c
@@ -14,6 +14,7 @@ int crypt(KEY key, const char* in, char* out, int modus)
     char* outPos = out;
     int pos = -1;
 
+    /** Check if the key is to short **/
     if(!strlen(key.chars))
     {
         return E_KEY_TOO_SHORT;
@@ -21,11 +22,13 @@ int crypt(KEY key, const char* in, char* out, int modus)
 
     for(inPos = in; *inPos; ++inPos)
     {
+        /** Reuse the key, if the input is longer than the key **/
         if(!*keyPos)
         {
             keyPos = key.chars;
         }
 
+        /** Check if the key contain illegal characters **/
         if(!strchr(KEY_CHARACTERS, *keyPos))
         {
             return E_KEY_ILLEGAL_CHAR;
@@ -33,22 +36,26 @@ int crypt(KEY key, const char* in, char* out, int modus)
 
         if(modus == ENCRYPT_MODUS)
         {
+            /** Check if the message contain illegal characters **/
             if(!strchr(MESSAGE_CHARACTERS, *inPos))
             {
                 return E_MESSAGE_ILLEGAL_CHAR;
             }
 
+            /** Xor the message position with the key position **/
             pos = ((strchr(MESSAGE_CHARACTERS, *inPos) - (MESSAGE_CHARACTERS)) + 1) ^
                   ((strchr(KEY_CHARACTERS, *keyPos) - (KEY_CHARACTERS)) + 1);
             *outPos = CYPHER_CHARACTERS[pos];
         }
         else
         {
+            /** Check if the cypher contain illegal characters **/
             if(!strchr(CYPHER_CHARACTERS, *inPos))
             {
                 return E_CYPHER_ILLEGAL_CHAR;
             }
 
+            /** Xor the cypher position with the key position **/
             pos = ((strchr(CYPHER_CHARACTERS, *inPos) - (CYPHER_CHARACTERS))) ^
                   ((strchr(KEY_CHARACTERS, *keyPos) - (KEY_CHARACTERS)) + 1);
             *outPos = MESSAGE_CHARACTERS[pos - 1];
